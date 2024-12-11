7f16eba0039fe97508cab27f80593ae127608599
pyarivisva <pyarivisvapujita1@gmail.com>
2024-12-11 14:37:15 +0800

    nambahin route lesson teacher

--- routes/web.php
+++ routes/web.php
@@ -41,8 +41,11 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/profile/update', [App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
     
     Route::get('/dashboard/teachers', [DashboardController::class, 'dashboardTeachers'])->name('dashboard-teachers');
+
     Route::get('/dashboard/classes-teachers', [TeacherClassesController::class, 'index'])->name('classes-teachers');
     Route::get('/dashboard/lessons-teachers/{id}', [TeacherClassesController::class, 'show'])->name('classes.lessons-teachers');
+    Route::get('/lessons/{classId}/{lessonsId}', [ClassesController::class, 'showDetail'])->name('lessons.detail');
+    
     Route::get('/assignments{id}', [TeacherAssignmentsController::class, 'index'])->name('assignments.index');
     
 });
